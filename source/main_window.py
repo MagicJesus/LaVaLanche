8df77831de23f6a3401f844437cbc73a338a83bd
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2020-04-20 16:25:18 +0200

    opytmizacje lekkie

--- source/main_window.py
+++ source/main_window.py
@@ -7,12 +7,12 @@ from PyQt5.QtGui import QImage, QPalette, QBrush, QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
-class MapWindow: # klasa reprezentujaca okienko z mapa na której beda odnośniki do każdej z map
+class MapWindow(QMainWindow): # klasa reprezentujaca okienko z mapa na której beda odnośniki do każdej z map
     def __init__(self):
-        self.app = QApplication(sys.argv)
-        self.win = QMainWindow()
-        self.win.setGeometry(600, 600, 753, 454)
-        self.win.setWindowTitle("LaVaLanche")
+        super(MapWindow, self).__init__()
+        # self.buttons =
+        self.setGeometry(600, 600, 753, 454)
+        self.setWindowTitle("LaVaLanche")
         self.set_image(os.path.join('images', 'tlo.png'))
 
     def set_image(self, img_path):
@@ -20,46 +20,54 @@ class MapWindow: # klasa reprezentujaca okienko z mapa na której beda odnośnik
         simage = oimage.scaled(QSize(1506, 908))
         palette = QPalette()
         palette.setBrush(QPalette.Window, QBrush(oimage))
-        self.win.setPalette(palette)
+        self.setPalette(palette)
 
-    def draw_map_link_buttons(self): # uzywajac button_map.txt jakos te przyciski naloze na mapke :P
-        pass
-    # w pliku maps_processing moga byc funkcje zwiazane z przetwarzaniem map
+    # def draw_map_link_buttons(self): # uzywajac button_map.txt jakos te przyciski naloze na mapke :P
+    #     initial_y_coord = 80
+    #     button_width = 21
+    #     button_height = 22
+    #     with open("button_map.txt") as buttonmap:
+    #         for line in buttonmap:
+    #             linia = line
+    #             for ch in linia:
+    #                 initial_x_coord = 80
+    #                 if ch == "0": #jak 0 to skocz do nastepnego miejsca
+    #                     initial_x_coord += button_width + 1
+    #                     continue
+    #                 elif ch == "1": #jak 1 to stworz guzik
 
 
-class MainWindow: # klasa reprezentujaca glowne okno aplikacji
+class MainWindow(QMainWindow): # klasa reprezentujaca glowne okno aplikacji
     def __init__(self):
-        self.app = QApplication(sys.argv)
-        self.win = QMainWindow()
-        self.win.setGeometry(500, 500, 753, 454)
-        self.win.setWindowTitle("LaVaLanche")
+        super(MainWindow, self).__init__()
+        self.setGeometry(500, 500, 753, 454)
+        self.setWindowTitle("LaVaLanche")
         self.dialogs = list()
         self.draw_labels()
-
-        self.show_map_button = QPushButton(self.win)
-        self.show_map_button.setText("SHOW HAZARD MAP")
-        self.show_map_button.setGeometry(300, 100, 150, 30)
-        self.show_map_button.clicked.connect(self.show_map) # .clicked.connect() przy przycisnieciu wola funkcje
-        self.win.show()
+        self.draw_buttons()
+        self.show()
 
     def draw_buttons(self):
-        pass
+        self.show_map_button = QPushButton(self)
+        self.show_map_button.setText("SHOW HAZARD MAP")
+        self.show_map_button.setGeometry(300, 100, 150, 30)
+        self.show_map_button.clicked.connect(self.show_map)  # .clicked.connect() przy przycisnieciu wola funkcje
 
     def set_image(self, img_path):
         oimage = QImage(img_path)
         simage = oimage.scaled(QSize(753, 454))
         palette = QPalette()
         palette.setBrush(QPalette.Window, QBrush(simage))
-        self.win.setPalette(palette)
+        self.setPalette(palette)
 
     def draw_labels(self):
-        self.label = QtWidgets.QLabel(self.win)
+        self.label = QtWidgets.QLabel(self)
         self.font = QFont("Times", 20, QFont.Bold)
         self.label.setGeometry(170, 0, 500, 50)
         self.label.setFont(self.font)
         self.label.setText("Witaj w (TEMPLATE APP NAME)")
 
-        self.label2 = QtWidgets.QLabel(self.win)
+        self.label2 = QtWidgets.QLabel(self)
         self.font = QFont("Times", 20,)
         self.label2.setText("<font color='white'>Made by JachyCzkowicz Enterprises 2020</font>")
         self.label2.setGeometry(10, 430, 300, 20)
@@ -67,13 +75,14 @@ class MainWindow: # klasa reprezentujaca glowne okno aplikacji
     def show_map(self): # funkcja wołająca się gdy przyciskamy przycisk "POKAZ MAPE ZAGROZEN"
         mapa = MapWindow() # tworzenie instancji klasy MapWindow()
         self.dialogs.append(mapa) # dodanie okna z mapą do dialogów głównego okna MainWindow
-        mapa.win.show()
+        mapa.show()
 
     def klikniecie(self):
         print("dupa")
 
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
     mainwindow = MainWindow()
     mainwindow.set_image(os.path.join('images', 'main_bg.jpg'))
-    sys.exit(mainwindow.app.exec_())
+    sys.exit(app.exec_())
