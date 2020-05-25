353459b572a40270d52e51ace345612a6b2e31e8
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2020-05-25 17:39:42 +0200

    poprawione sciezki v1

--- source/weather_conditions.py
+++ source/weather_conditions.py
@@ -25,36 +25,85 @@ def extract_coords(map_name):
 
 
 def get_weather_conditions(map_name):
-    # stworz odpowiedni folder jeżeli nie istnieje
+    # create a folder if id does not exist
     if os.path.isdir("..\\data\\weather_data\\" + map_name[0:16]):
         pass
     else:
         path = "..\\data\\weather_data\\" + map_name[0:16]
         os.mkdir(path)
 
-    # współrzędne obszaru i aktualny czas
+    # coordinates of a given area and the time of measurement
     coords = extract_coords(map_name)
     czas_pomiaru = str(time.ctime()).split(" ")
 
-    # inicjalizacja zmiennej pogodowej hehe i pobranie pogody
+    # getting the weather data
     owm = pyowm.OWM("7202a85833f71127c0a0b4fefc86ea2a")
     observation = owm.weather_around_coords(float(coords["N_lat"]), float(coords["W_long"]))
 
-    # sprawdz czy bylo 6 pomiarów, jak bylo usun najstarszy
+    # check if there were 6 measurements, if yes, delete the oldest one
     data = os.listdir("..\\data\\weather_data\\" + map_name[0:16] + "\\")
     if len(data) >= 6:
         os.remove("..\\data\\weather_data\\" + map_name[0:16] + "\\" + data[0])
-    # stworz plik z danymi z konkretnego dnia i zapisz w pliku
+    # create a file containing up to date weather data
     filename = str(observation[0].get_weather().get_reference_time('date'))[0:10]
     data = open("..\\data\\weather_data\\" + map_name[0:16] + "\\" + filename + "_" + czas_pomiaru[3].replace(":", "-") + ".txt", "w+")
-    data.write("Temperatura: " + str(observation[0].get_weather().get_temperature('celsius')['temp']) + "\n")
-    data.write("Opad sniegu: " + str(observation[0].get_weather().get_snow()) + "\n")
-    data.write("Wiatr[m/s]: " + str(observation[0].get_weather().get_wind('meters_sec')) + "\n")
-    data.write("Opad deszczu: " + str(observation[0].get_weather().get_rain()) + "\n")
+    data.write(str(observation[0].get_weather().get_temperature('celsius')['temp']) + "\n")
+    data.write(str(observation[0].get_weather().get_snow()) + "\n")
+    data.write(str(observation[0].get_weather().get_wind('meters_sec')) + "\n")
+    data.write(str(observation[0].get_weather().get_rain()) + "\n")
     print("end")
 
 
-with open("..\\data\\lats_longs_scraped.txt") as file:
-    file.readline()
-    for line in file:
-        get_weather_conditions(line.rstrip()[0:16])
+# with open("..\\data\\lats_longs_scraped.txt") as file:
+#     file.readline()
+#     for line in file:
+#         get_weather_conditions(line.rstrip()[0:16])
+
+
+def weather_analysis(map_name):
+    path = "..\\data\\weather_data\\" + map_name[0:16]
+    weather = {}
+    temperature = []
+    snow = []
+    wind = []
+    rain = []
+    files = os.listdir(path)
+    for file in files:
+        handle = open(path + "\\" + file)
+        temperature.append(handle.readline().rstrip())
+        snow.append(handle.readline().rstrip())
+        wind.append(handle.readline().rstrip())
+        rain.append(handle.readline().rstrip())
+        handle.close()
+
+    # temperature stats
+    mean = []
+    for t in temperature:
+        mean.append(float(t))
+    avg = sum(mean)/len(mean)
+    if min(mean)*max(mean) < 0 and avg > 0:
+        weather["wzr_temp"] = True
+    else:
+        weather["wzr_temp"] = False
+    # snow stats
+    weather["snieg48h"] = False
+    for s in snow:
+        if s.replace("{}", "none") != "none":
+            weather["snieg48h"] = True
+    # rain stats
+    weather["deszcz48h"] = False
+    for r in rain:
+        if s.replace("{}", "none") != "none":
+            weather["deszcz48h"] = True
+    # wind stats
+    weather["wiatr48h"] = False
+    for w in wind:
+        if float(w[10:13]) >= 13.0:
+            weather["wiatr48h"] = True
+
+    print(map_name, weather)
+
+
+mapy = os.listdir("D:\\Mapy Tatr\\A")
+for m in mapy:
+    weather_analysis(m)
