7874e1e5381bdff1fd81bd2e4c707ed4662b9fdd
dittohed <wojtek.konieczkowicz@gmail.com>
2020-06-03 00:19:17 +0200

    blank message

--- data/scrape_obiekty.py
+++ data/scrape_obiekty.py
@@ -1,5 +1,6 @@
-# Ten skrypt pobiera informacje dotyczące ewentualnego występowania żlebów na obszarach jednostkowych
+# Ten skrypt pobiera informacje dotyczące występowania obiektów na obszarach jednostkowych
 # korzystając z pliku links.html zawierającego odpowiednie linki do stron dostawcy plików las.
+# Obiekty to np. szczyty i znane miejsca, które ułatwiają orientację, jakiego obszaru dotyczy zagrożenie.
 
 from selenium import webdriver # zautomatyzowane użycie przeglądarki
 from bs4 import BeautifulSoup # analiza kodu HTML
@@ -8,7 +9,7 @@ import re # regexp
 from create_coord_dict import get_centers
 coord_dict = get_centers()
 
-f = open("zleby_scraped.txt", "w")
+f = open("obiekty_scraped.txt", "w")
 
 # --- OTWARCIE W PRZEGLĄDARCE PLIKU links.html I POBRANIE ZAWARTOŚCI STRONY ---
 driver = webdriver.Chrome("/usr/bin/chromedriver")
@@ -36,22 +37,24 @@ for link in soup.find_all('a'):
             content = driver_2.page_source
             soup_temp2 = BeautifulSoup(content, features = "lxml")
 
-            zleb_name = soup_temp2.body.findAll(text = re.compile(" Żleb$")) # czy to strona dotycząca żlebu?
-            if zleb_name:
-                # oblicz koordynaty i wyznacz odpowiednią nazwę obszaru jednostkowego
-                coords = soup_temp2.body.findAll(text = re.compile("[1-8][0-9]°"))
-                lat = float(coords[0].split(' ')[0])
-                long = float(coords[1].split(' ')[0])
+            object_name = soup_temp2.title.text
 
-                if lat > lat_center:
-                    if long > long_center:
-                        sub_area = "-2"
-                    else:
-                        sub_area = "-1"
+            # oblicz koordynaty i wyznacz odpowiednią nazwę obszaru jednostkowego, na którym znajduje się obiekt
+            coords = soup_temp2.body.findAll(text = re.compile("[1-8][0-9]°"))
+            lat = float(coords[0].split(' ')[0])
+            long = float(coords[1].split(' ')[0])
+
+            if lat > lat_center:
+                if long > long_center:
+                    sub_area = "-2"
                 else:
-                    if long > long_center:
-                        sub_area = "-4"
-                    else:
-                        sub_area = "-3"
+                    sub_area = "-1"
+            else:
+                if long > long_center:
+                    sub_area = "-4"
+                else:
+                    sub_area = "-3"
 
-                f.write(area_name + sub_area + ',' + zleb_name[0] + '\n')
+            to_write = area_name + sub_area + ',' + object_name + '\n'
+            print(to_write)
+            f.write(to_write)
