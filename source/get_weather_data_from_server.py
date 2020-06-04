3935faee990a24cb0c4d3345e8af9eb79c750559
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2020-06-04 19:17:05 +0200

    poprawiona analiza pogody

--- source/get_weather_data_from_server.py
+++ source/get_weather_data_from_server.py
@@ -1,22 +1,22 @@
 from paramiko import SSHClient
 from scp import SCPClient
-import tarfile, os, shutil
+import tarfile, os, shutil, time
 
+overall_time = time.time()
+start_time = time.time()
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect('34.72.152.18', username="marekjachym99")
+print("ssh connection took: ", time.time() - start_time)
 
+start_time = time.time()
 # SCPCLient takes a paramiko transport as an argument
 scp = SCPClient(ssh.get_transport())
-
 scp.get("/home/marekjachym99/lavalanche/weather.tar", "D:\\GitHub\\LaVaLanche\\data")
-
-# Uploading the 'test' directory with its content in the
-# '/home/user/dump' remote directory
-
 scp.close()
-
+print("scp copying took: ", time.time() - start_time)
 # extract .tar file
+start_time = time.time()
 archive = tarfile.open("..\\data\\weather.tar", "r")
 archive.extractall("..\\data\\")
 archive.close()
@@ -24,3 +24,5 @@ shutil.rmtree("..\\data\\weather_data")
 shutil.move("..\\data\\home\\marekjachym99\\lavalanche\\weather_data", "..\\data\\")
 os.remove("..\\data\\weather.tar")
 shutil.rmtree("..\\data\\home")
+print("file handling took: ", time.time() - start_time)
+print("whole process took: ", time.time() - overall_time)
--- source/weather_analysis.py
+++ source/weather_analysis.py
@@ -1,9 +1,10 @@
 import pyowm
-import os
+import os, re
 import time
 
 
 def weather_analysis(map_name):
+    regex_pattern = '[0-9]+\.?[0-9]*'
     path = "..\\data\\weather_data\\" + map_name[0:16]
     weather = {}
     temperature = []
@@ -33,9 +34,7 @@ def weather_analysis(map_name):
     overall_snow_layer = 0.0
     for s in snow:
         if 'h' in s:
-            amount_in_mm = s[7:11]
-            if '}' in amount_in_mm:
-                amount_in_mm = amount_in_mm[0:3]
+            amount_in_mm = re.findall(regex_pattern, s)[1]
             overall_snow_layer += float(amount_in_mm)
         elif s.replace("{}", "none") == "none":
             overall_snow_layer += 0
@@ -45,12 +44,12 @@ def weather_analysis(map_name):
     # rain stats
     weather["deszcz48h"] = False
     for r in rain:
-        if s.replace("{}", "none") != "none":
+        if '{}' not in r:
             weather["deszcz48h"] = True
     # wind stats
     weather["wiatr48h"] = False
     for w in wind:
-        if float(w[10:13]) >= 13.0:
+        if float(re.findall(regex_pattern, w)[0]) >= 13.0:
             weather["wiatr48h"] = True
 
     print(map_name, weather)
