35cbca269fd6d79c9d419a9f65564449da387f99
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2020-06-04 11:06:00 +0200

    download weather final version

--- source/get_weather_data_from_server.py
+++ source/get_weather_data_from_server.py
@@ -1,5 +1,6 @@
 from paramiko import SSHClient
 from scp import SCPClient
+import tarfile, os, shutil
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
@@ -8,9 +9,18 @@ ssh.connect('34.72.152.18', username="marekjachym99")
 # SCPCLient takes a paramiko transport as an argument
 scp = SCPClient(ssh.get_transport())
 
-scp.get("/home/marekjachym99/lavalanche/weather_data", "D:\\GitHub\\LaVaLanche\\data", recursive=True)
+scp.get("/home/marekjachym99/lavalanche/weather.tar", "D:\\GitHub\\LaVaLanche\\data")
 
 # Uploading the 'test' directory with its content in the
 # '/home/user/dump' remote directory
 
 scp.close()
+
+# extract .tar file
+archive = tarfile.open("..\\data\\weather.tar", "r")
+archive.extractall("..\\data\\")
+archive.close()
+shutil.rmtree("..\\data\\weather_data")
+shutil.move("..\\data\\home\\marekjachym99\\lavalanche\\weather_data", "..\\data\\")
+os.remove("..\\data\\weather.tar")
+shutil.rmtree("..\\data\\home")
--- source/weather_conditions.py
+++ source/weather_conditions.py
@@ -3,63 +3,6 @@ import os
 import time
 
 
-def extract_coords(map_name):
-
-    if ".las" in map_name:
-        map_name = map_name[0:16]
-    else:
-        map_name = map_name[0:16]
-    file = open("..\\data\\lats_longs_scraped.txt")
-    for line in file:
-        if map_name in line:
-            line = line[0:len(line) - 1]
-            splited = line.split(",")
-            coords = {
-                    "N_lat": splited[1],
-                    "S_lat": splited[2],
-                    "W_long": splited[3],
-                    "E_long": splited[4]
-                    }
-            break
-    return coords
-
-
-def get_weather_conditions(map_name):
-    # create a folder if it does not exist
-    if os.path.isdir("..\\data\\weather_data\\" + map_name[0:16]):
-        pass
-    else:
-        path = "..\\data\\weather_data\\" + map_name[0:16]
-        os.mkdir(path)
-
-    # coordinates of a given area and the time of measurement
-    coords = extract_coords(map_name)
-    czas_pomiaru = str(time.ctime()).split(" ")
-
-    # getting the weather data
-    owm = pyowm.OWM("7202a85833f71127c0a0b4fefc86ea2a")
-    observation = owm.weather_around_coords(float(coords["N_lat"]), float(coords["W_long"]))
-
-    # check if there were 6 measurements, if yes, delete the oldest one
-    data = os.listdir("..\\data\\weather_data\\" + map_name[0:16] + "\\")
-    if len(data) >= 6:
-        os.remove("..\\data\\weather_data\\" + map_name[0:16] + "\\" + data[0])
-    # create a file containing up to date weather data
-    filename = str(observation[0].get_weather().get_reference_time('date'))[0:10]
-    data = open("..\\data\\weather_data\\" + map_name[0:16] + "\\" + filename + "_" + czas_pomiaru[3].replace(":", "-") + ".txt", "w+")
-    data.write(str(observation[0].get_weather().get_temperature('celsius')['temp']) + "\n")
-    data.write(str(observation[0].get_weather().get_snow()) + "\n")
-    data.write(str(observation[0].get_weather().get_wind('meters_sec')) + "\n")
-    data.write(str(observation[0].get_weather().get_rain()) + "\n")
-    print("end")
-
-#
-# with open("..\\data\\lats_longs_scraped.txt") as file:
-#     file.readline()
-#     for line in file:
-#         get_weather_conditions(line.rstrip()[0:16])
-
-
 def weather_analysis(map_name):
     path = "..\\data\\weather_data\\" + map_name[0:16]
     weather = {}
